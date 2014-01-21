670f1ab2a79a50c76d2600c77f3452f879e6f023
Sidirius <sid@sh87.net>
2014-01-21 21:43:36 +0100

    Fehler in cron behoben

--- create_cgminer_cron.sh
+++ create_cgminer_cron.sh
@@ -2,7 +2,7 @@
 #write out current crontab
 crontab -l > /tmp/setup_cgminer/cgminer_tmp.cron
 #echo new cron into cron file
-echo "@reboot screen -dmS cgminer /home/pi/cgminer/cgminer --config /home/pi/cgminer/cgminer.conf" >> /tmp/setup_cgminer/cgminer_tmp.cron
+echo "@reboot screen -dmS cgminer ~/cgminer/cgminer --config ~/cgminer/cgminer.conf" >> /tmp/setup_cgminer/cgminer_tmp.cron
 #install new cron file
 crontab /tmp/setup_cgminer/cgminer_tmp.cron
 rm /tmp/setup_cgminer/cgminer_tmp.cron
